119b87461746e7efade9168922e238818155a810
mostafmohmed <01117429622mo@gmail.com>
2025-03-03 11:53:01 +0200

    update task

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -21,7 +21,7 @@ class CartController extends Controller
         
         
   foreach($Produects as $k=> $Produect ){
-    if ($Produect->quantity==0) {
+    if ($Produect->quantity<=0) {
       return apiresponse(404,' not available in stock');
     }
     Cart::add(['id' =>  $Produect->id, 'name' =>app()->getLocale()=='en'? $Produect->name_en:$Produect->name_ar, 'qty' => 1, 'price' => $Produect->price]);
